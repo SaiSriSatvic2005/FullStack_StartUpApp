f99c505a149129d26fff014984dd599796065eca
satvic <saisrisatvic@gmail.com>
2025-09-14 02:44:49 +0530

    modified nextconfigts

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,12 @@ import {withSentryConfig} from '@sentry/nextjs';
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+  typescript:{
+    ignoreBuildErrors:true,
+  },
+  eslint:{
+    ignoreDuringBuilds:true,
+  },
   images: {
     dangerouslyAllowSVG:true,
     remotePatterns: [
